e023b540a8039e1c72a4fc573b42c3889b0bf82a
alliecollins <allisonleecollins@gmail.com>
2020-03-01 18:59:50 -0600

    uploading current progress

--- src/app.js
+++ src/app.js
@@ -5,24 +5,21 @@
 const domReady = require('domready');
 import * as d3 from "d3";
 
-
 // DATA LOADING
 
-// domReady(() => {
-//   // this is just one example of how to import data. there are lots of ways to do it!
+domReady(() => {
+  // this is just one example of how to import data. there are lots of ways to do it!
 
-//   fetch('./data/sample_output.json')
-//     .then(response => response.json())
-//     .then(data => Map(data))
-//     .catch(e => {
-//       console.log(e);
-//     });
-// });
+  fetch('./data/sample_output.json')
+    .then(response => response.json())
+    .then(data => makeMap(data))
+    .catch(e => {
+      console.log(e);
+    });
+});
 
-// get map data
-d3.json(
-  './data/sample_output.json').then(
-  function(json) {
+
+function makeMap(json) {
     console.log(json)
     const width = 1000;
     const height = 600;
@@ -37,103 +34,41 @@ d3.json(
       .attr('width', margin.left + width + margin.right)
       .attr('height', margin.top + height + margin.bottom);
 
-    // projection = d3.geo.projection(function(x, y) { return [x, y];}).precision(0).scale(1).translate([0, 0]);
+    //citation for the identity projection function: https://bl.ocks.org/KingOfCramers/f3a2f8de1cfbbb55d9ca75d1335c8a73
     var projection = d3.geoIdentity().reflectY(true).fitSize([width,height], json)
-
-
-    //var projection = d3.geoEquirectangular().translate([width/2, height/2]);
     var path = d3.geoPath().projection(projection);
 
-    //var geoGenerator = d3.geoPath().projection();
+    var tooltip = d3.select("#map").append("div") 
+    .attr("id", "tooltip")       
+    .style("opacity", 0);
+
+    var colorScale = d3.scaleThreshold()
+    .domain([0, 10, 20, 40, 50, 100])
+    .range(d3.schemeBlues[7]);
     
     svg.selectAll('path')
       .data(json.features)
       .enter()
       .append('path')
       .attr('d', path)
-      //.attr('d', d => geoGenerator(d))
-     /////////////////////////////////////////////
-     //////// Here we will put a lot of code concerned
-     //////// with drawing the map. This will be defined
-     //////// in the next sections.
-     /////////////////////////////////////////////
+      .attr("fill", function (d) {
+        d.total = d.properties['HIV diagnoses'] || 0;
+        return colorScale(d.total);
+      })
+      .on("mouseover", function(d) {
+      d3.select(this)    
+      tooltip.transition()    
+      .duration(200)   
+      .style("opacity", 1);    
+      tooltip.html(d.properties.UHF_NEIGH)  
+      .style("display", "block")   
+    })          
+    .on("mouseout", function(d) {   
+      tooltip.transition()    
+      .duration(500)    
+      .style("opacity", 0); 
+    });
   }
-);
-
-// EXAMPLE FIRST content FUNCTION
-
-// function myVis(data) {
-//   // portrait
-//   const width = 5000;
-//   const height = (36 / 24) * width;
-//   console.log(data, height);
-//   console.log('Hi!');
-// }
-
-// const map = 
-// function makeMap(data) {
-//   console.log("hello!")
-//     // const width = 1000;
-//     // const height = 600;
-//     // const margin = {
-//     //   top: 20,
-//     //   left: 20,
-//     //   right: 20,
-//     //   bottom: 10
-//     // };
-
-//     var projection = d3.geoEquirectangular();
-//     var geoGenerator = d3.geoPath()
-//     .projection(projection);
-
-//     // Join the FeatureCollection's features array to path elements
-//     var u = d3.select('#map g.map')
-//     .selectAll('path')
-//     console.log(data.features)
-//     .data(data.features);
-//     // Create path elements and update the d attribute using the geo generator
-//     u.enter()
-//     .append('path')
-//     .attr('d', geoGenerator);
-
-//     svg = d3.select('#map').append('svg')
-//       .attr('width', margin.left + width + margin.right)
-//       .attr('height', margin.top + height + margin.bottom);
-//     }
-
-// function Map = {
-//   // initialize the pastry
-//   init: function (data) {
-//   },
-// }
-
-// function Map (data) {
-//   this.data = data
-// }
-
-// Map function(data) {
-//   console.log("am I getting here")
-//   this.data = data
-//   //this.makeMap(data);
-// };
-
-// Map.prototype = {
-//   makeMap: function (data) {
-//     console.log("Am I getting here")
-//     const width = 1000;
-//     const height = 600;
-//     const margin = {
-//       top: 20,
-//       left: 20,
-//       right: 20,
-//       bottom: 10
-//     };
-
-//     svg = d3.select('#map').append('svg')
-//       .attr('width', margin.left + width + margin.right)
-//       .attr('height', margin.top + height + margin.bottom);
-
-//     geoGenerator = d3.geoPath().projection(d3.geoEquirectangular);
     
 //     svg.selectAll('path')
 //       .data(data.features)
@@ -153,7 +88,6 @@ d3.json(
 // //   // //     .attr('fill', d => fips2Value[d.properties.GEOID])
 // //   // //     .attr('opacity', 1);
 
-// //   // // }
 // };
 
 //;
