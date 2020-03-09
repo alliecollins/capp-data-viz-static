39d046edacbcd77408e30e02d3eb69d6225e9ee7
alliecollins <allisonleecollins@gmail.com>
2020-03-08 22:17:20 -0500

    pushing latest

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 //IMPORT STATEMENTS
-const domReady = require('domready');
+const domReady = require("domready");
 import * as d3 from "d3";
-import {sliderHorizontal} from 'd3-simple-slider'
+import { sliderHorizontal } from "d3-simple-slider";
 
 // DATA LOADING
 domReady(() => {
-
-  fetch('./data/geoData.json')
+  fetch("./data/finalData.json")
     .then(response => response.json())
     .then(data => runAll(data))
     //.then(data => makeJitter(data))
@@ -15,121 +14,150 @@ domReady(() => {
     });
 });
 
-
 //WRAPPER FUNCTON
 function runAll(json) {
-  makeMap(json, 2011, "All")
-  makeJitter(json)
-  makeSlider(json)
-  makeDropDown(json)
+  const state = {
+    year: 2011,
+    race: "All"
+  };
+  makeMap(json, state.year, state.race);
+  makeJitter(json);
+  makeSlider(json, function(d) {
+    d3.select('#map').selectAll("*").remove();
+    state.year = d;
+    makeMap(json, state.year, state.race);
+  });
+  makeDropDown(json, function(d) {
+    d3.select('#map').selectAll("*").remove();
+    state.race = this.value;
+    makeMap(json, state.year, state.race);
+  });
 }
-
 
 // CREATE MAP
 function makeMap(json, year, race) {
+  const width = 1000;
+  const height = 600;
+  const margin = {
+    top: 20,
+    left: 20,
+    right: 20,
+    bottom: 10
+  };
 
-    const width = 1000;
-    const height = 600;
-    const margin = {
-      top: 20,
-      left: 20,
-      right: 20,
-      bottom: 10
-    };
+  var svg = d3
+    .select("#map")
+    .append("svg")
+    .attr("width", margin.left + width + margin.right)
+    .attr("height", margin.top + height + margin.bottom)
+    .selectAll("path")
+    .data(json.features);
 
-    var svg = d3.select('#map').append('svg')
-      .attr('width', margin.left + width + margin.right)
-      .attr('height', margin.top + height + margin.bottom)
-      .selectAll('path')
-      .data(json.features);
+  //citation for the identity projection function: https://bl.ocks.org/KingOfCramers/f3a2f8de1cfbbb55d9ca75d1335c8a73
+  var projection = d3
+    .geoIdentity()
+    .reflectY(true)
+    .fitSize([width, height], json);
+  var path = d3.geoPath().projection(projection);
 
-    //citation for the identity projection function: https://bl.ocks.org/KingOfCramers/f3a2f8de1cfbbb55d9ca75d1335c8a73
-    var projection = d3.geoIdentity().reflectY(true).fitSize([width,height], json)
-    var path = d3.geoPath().projection(projection);
-
-    //citation for chloropleth: https://www.d3-graph-gallery.com/graph/choropleth_hover_effect.html
-    var colorScale = d3.scaleThreshold()
-    .domain([0, 10, 20, 40, 50, 75, 100])
+  //citation for chloropleth: https://www.d3-graph-gallery.com/graph/choropleth_hover_effect.html
+  var colorScale = d3
+    .scaleThreshold()
+    .domain([0, 5, 10, 20, 30, 50, 75, 100, 125, 175])
     .range(d3.schemeBlues[7]);
-    
-    svg.enter()
-    .append('path')
-    .filter(function(d) { return d.properties.Year === year})
-    //.filter(function(d) { return d.properties.Year === year || d.properties.Race == race})
-    //.filter(function(d) { return d.properties.Race === race})
-    .attr('d', path)
-    .attr("fill", function (d) {
-        d.total = d.properties['HIV diagnoses'] || 0;
-        return colorScale(d.total);
-      })
-      .on("mouseover", function(d) {
-      d3.select("#tooltip")    
-      .style("opacity", 1)
-      .style('left', d3.event.pageX + 'px')
-      .style('top', d3.event.pageY + 'px')
-      .text(d.properties.UHF_NEIGH)  
-    })          
-    .on("mouseout", function(d) { 
-      d3.select("#tooltip").style('opacity', 0)  
+
+  svg
+    .enter()
+    .append("path")
+    .filter(function(d) {
+      return d.properties.Year === year || d.properties.Race == race;
+    })
+    .attr("d", path)
+    .attr("fill", function(d) {
+      d.total = d.properties["HIV diagnosis rate"] || 0;
+      return colorScale(d.total);
+    })
+    .on("mouseover", function(d) {
+      d3.select("#tooltip")
+        .style("opacity", 1)
+        .attr("width", 300)
+        .attr("height", 200)
+        .style("left", d3.event.pageX + "px")
+        .style("top", d3.event.pageY + "px")
+        .text(d.properties.UHF_NEIGH);
+    })
+    .on("mouseout", function(d) {
+      d3.select("#tooltip").style("opacity", 0);
     });
 
-    svg.exit().remove();
-
-    //add tooltip
-    d3.select('body')
-    .append('div')
-    .attr('id', 'tooltip')
+  //add tooltip
+  d3.select("#tooltip")
     .attr("width", 300)
-    .attr('height', 200)
-    .attr('style', 'position: absolute; opacity: 0;');
-  }
+    .attr("height", 200)
+    .attr("style", "position: absolute; opacity: 0;");
+}
 
-  // CREATE JITTER - citation for basic scatterplot code: https://bl.ocks.org/kheaney21/5649ddce43f3005fc523b027d503bc3d
+// CREATE JITTER - citation for basic scatterplot code: https://bl.ocks.org/kheaney21/5649ddce43f3005fc523b027d503bc3d
 function makeJitter(json) {
   var width = 1000;
   var height = 100;
   var padding = 20;
 
-  var svg = d3.select('#jitter').append('svg')
+  var svg = d3
+    .select("#jitter")
+    .append("svg")
     .attr("width", width)
     .attr("height", height);
 
   var jitterWidth = 4;
-  var xScale = d3.scaleLinear().domain([0, 200]).range([padding, width - padding * 2]);
+  var xScale = d3
+    .scaleLinear()
+    .domain([0, 200])
+    .range([padding, width - padding * 2]);
   var xAxis = d3.axisBottom(xScale);
-  var yScale = d3.scaleLinear().domain([0, 5]).range([height - padding, padding]);
+  var yScale = d3
+    .scaleLinear()
+    .domain([0, 5])
+    .range([height - padding, padding]);
 
-    // x-axis
+  // x-axis
 
-  svg.selectAll("circle")
+  svg
+    .selectAll("circle")
     .data(json.features)
     .enter()
     .append("circle")
     .attr("class", "dot")
-    .attr("cy", function () {
-      return yScale((Math.random()*jitterWidth));
+    .attr("cy", function() {
+      return yScale(Math.random() * jitterWidth);
     })
-    .attr("cx", function (d) {
-        d.total = d.properties['HIV diagnoses'] || 0
-        return xScale(d.total);})
+    .attr("cx", function(d) {
+      d.total = d.properties["HIV diagnosis rate"] || 0;
+      return xScale(d.total);
+    })
     .attr("r", 2.5)
     .style("fill", "steelblue");
 
-  svg.append("g")
+  svg
+    .append("g")
     .attr("class", "x axis")
-    .attr("transform", "translate(0," + (height- padding) + ")")
+    .attr("transform", "translate(0," + (height - padding) + ")")
     .call(xAxis);
 
-  svg.append("text")
+  svg
+    .append("text")
     .attr("class", "x label")
     .attr("text-anchor", "end")
-    .attr("transform", "translate(" + (width / 2) + " ," + (height + padding) + ")")
+    .attr(
+      "transform",
+      "translate(" + width / 2 + " ," + (height + padding) + ")"
+    )
     .text("income per capita, inflation-adjusted (dollars)");
 }
 
 //ADD INTERACTIVE TOOLS
 
-function makeSlider(json) {
+function makeSlider(json, onChange) {
   // Initialize slider
 
   var sliderSimple = sliderHorizontal()
@@ -138,50 +166,37 @@ function makeSlider(json) {
     .step(1)
     .width(300)
     .ticks(5)
-    .tickFormat(d3.format('.0f'))
+    .tickFormat(d3.format(".0f"))
     .default(2011)
-    .on('onchange', val => {
-      d3.select('#map').selectAll("*").remove();
-      var newYear = val
-      var currentRace = document.getElementById("selectButton").value
-      makeMap(json, newYear, currentRace)
-    });
+    .on("onchange", onChange);
 
   var gSimple = d3
-    .select('#slider')
-    .append('svg')
-    .attr('width', 500)
-    .attr('height', 100)
-    .append('g')
-    .attr('transform', 'translate(30,30)')
-
+    .select("#slider")
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 100)
+    .append("g")
+    .attr("transform", "translate(30,30)");
 
   gSimple.call(sliderSimple);
 }
 
-function makeDropDown(json) {
-// citation for code used: https://www.d3-graph-gallery.com/graph/line_select.html
-  var allGroup = ["All", "Asian/Pacific Islander", "Latino/Hispanic", "Black", "White"]
+function makeDropDown(json, onChange) {
+  // citation for code used: https://www.d3-graph-gallery.com/graph/line_select.html
 
-  // add the options to the button
-  var myButton = d3.select("#selectButton")
-    .selectAll('myOptions')
-    .data(allGroup)
+  d3.select("#selectButton")
+    .selectAll("myOptions")
+    .data([
+      "All",
+      "Asian/Pacific Islander",
+      "Latino/Hispanic",
+      "Black",
+      "White"
+    ])
     .enter()
-    .append('option')
-    .text(function (d) { return d; }) // text showed in the menu
-    .attr("value", function (d) { return d; }) // corresponding value returned by the button
-  
-  d3.select("select")
-  .on("change", function(d) {
-      //d3.select('#map').selectAll("*").remove();
-      var newRace = d3.select(this).property("value")
-      //var currentYear = document.getElementById("slider").value
-      console.log(newRace)
-      console.log(document.getElementById("slider").value)
-      //d3.select("#slider").call(slider)
-      // currentYear = slider.value
-      // console.log(currentYear)
-      //makeMap(json, currentYear, newRace)
-    });
+    .append("option")
+    .text(d => d) // text showed in the menu
+    .attr("value", d => d); // corresponding value returned by the button
+
+  d3.select("select").on("change", onChange);
 }
